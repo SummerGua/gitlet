80f886314b4dc3b554177f12609f5ed5455aaba5
summergua <1363106651@qq.com>
2024-02-06 19:51:05 +0800

    fix

--- utils/util.js
+++ utils/util.js
@@ -1,187 +1,101 @@
-const files = require("./files");
-const index = require("./index");
-const objects = require("./objects");
-const refs = require("./refs");
-const utils = require("../utils/util");
+const path = require("path");
+const zlib = require("zlib");
+const fs = require("fs");
+const crypto = require("crypto");
+const files = require("../src/files");
 
-const gitlet = {
-  GIT_DIR: ".gitlet",
-
-  init: (opts) => {
-    if (files.getGitFullPath(gitlet.GIT_DIR)) {
-      console.log(
-        "A gitlet version-control system already exists in the current directory."
-      );
-      return false;
-    }
-
-    opts = opts || { bare: false };
-
-    const gitletStruct = {
-      HEAD: "ref: refs/heads/master",
-      objects: {},
-      refs: {
-        heads: {},
-      },
-      config: JSON.stringify({ core: { bare: false } }, null, 2),
-    };
-
-    // if bare, don't create gitlet/
-    files.writeFilesFromTree(
-      opts.bare ? gitletStruct : { [gitlet.GIT_DIR]: gitletStruct },
-      process.cwd()
-    );
-  },
-
+const utils = {
   /**
-   * Adds a copy of the file as it currently exists to the *staging area*
-   * only save the up-to-date copy!
    *
-   * @param {string} workPath
-   * @returns {void}
-   */
-  add: (workPath) => {
-    if (!workPath) {
-      console.log("please input a path");
-      return;
-    }
-    if (!files.getGitFullPath(gitlet.GIT_DIR)) {
-      console.log("Not in a git repository.");
-      return;
-    }
-
-    const filesToAdd = files.findAllFiles(workPath, gitlet.GIT_DIR);
-    if (filesToAdd.length === 0) {
-      // not in work area
-      const ind = Object.keys(index.read(gitlet.GIT_DIR)).map((item) =>
-        utils.relToAbs(item)
-      );
-      const absPath = utils.relToAbs(workPath);
-      if (ind.includes(absPath)) {
-        // in index
-        console.log(`${workPath} removed from staging area`);
-        index.updateIndex([absPath], gitlet.GIT_DIR, { remove: true });
-      } else {
-        // neither in index nor in work area
-        console.log(`${workPath} did not match any files`);
-      }
-    } else {
-      // add to index and objects
-      index.updateIndex(filesToAdd, gitlet.GIT_DIR, { add: true });
-    }
-  },
-
-  rm: (workPath) => {
-    if (!files.getGitFullPath(gitlet.GIT_DIR)) {
-      console.log("Not in a git repository.");
-      return;
-    }
-
-    const filesToRemove = files.findAllFiles(workPath, gitlet.GIT_DIR);
-    if (filesToRemove.length === 0) {
-      console.log(`${workPath} did not match any files`);
-      return;
-    }
-
-    index.updateIndex(filesToRemove, gitlet.GIT_DIR, { remove: true });
-    files.removeFiles(filesToRemove);
-  },
-
-  commit: (message) => {
-    if (!files.getGitFullPath(gitlet.GIT_DIR)) {
-      console.log("Not in a git repository.");
-      return;
-    }
-
-    if (!message) {
-      console.log("Please enter a commit message");
-      return;
-    }
-
-    const treeHash = gitlet.write_tree();
-    const parentHash = refs.getParentHash();
-
-    if (parentHash !== "") {
-      const lastTreeHash = JSON.parse(
-        gitlet.cat_file(parentHash, "content", false)
-      ).tree;
-      if (treeHash === lastTreeHash) {
-        console.log("no changes added to commit");
-        return;
-      }
-    }
-
-    const commitHash = objects.createCommit(treeHash, parentHash, message);
-    // 把commit hash存到refs/heads/当前分支名
-    console.log(commitHash);
-    refs.updateRef(commitHash);
-  },
-
-  switch: (branchName) => {
-    if (!branchName) {
-      console.log("please pass a branch name");
-      return;
-    }
-    refs.changeBranch(branchName);
-  },
-
-  branch: (branchName) => {
-    if (branchName === undefined) {
-      const branch = refs.getCurrentBranch();
-      console.log(branch);
-      return branch;
-    }
-    refs.addBranch(branchName);
-  },
-
-  /**
-   * origin: git hash-object [-t \<type>] \<filename>
-   * NOTE: ONLY FILE can be passed, a dir can not
-   * @param {string} filename
+   * @param {string} content
    * @param {"blob" | "tree" | "commit"} type
-   * @returns {string} hashed object
+   * @returns {[string, string, string]} [hash[:2], hash[-38:], hash]
    */
-  hash_object: (filename, type) => {
-    if (!files.pathExists(filename)) {
-      console.log("No such file or directory");
-      return;
-    }
-    if (files.isDir(filename)) {
-      console.log("Please pass a file instead of a directory");
-      return;
-    }
-    const [_, __, hash] = utils.getFileHash(filename, type);
-    console.log(hash);
-    return hash;
+  getStrHash: (content, type = "blob") => {
+    /* git hash rule:
+     type + ' ' + contentSize + '\0' + content
+     NOTE: use byte concatenation instead of string concatenation
+    */
+    const hashBuffer = Buffer.concat([
+      Buffer.from(type), // default encoding = utf-8
+      Buffer.from(" "),
+      Buffer.from(content.length.toString()),
+      Buffer.from("\0"),
+      Buffer.from(content),
+    ]);
+
+    const hashSHA1 = crypto.createHash("sha1").update(hashBuffer).digest("hex");
+    const dirName = hashSHA1.substring(0, 2);
+    const fileName = hashSHA1.substring(hashSHA1.length - 38);
+
+    return [dirName, fileName, hashSHA1];
+  },
+
+  getFileHash: (filename) => {
+    if (!path.isAbsolute(filename)) filename = path.resolve(filename);
+    const content = fs.readFileSync(filename);
+    return utils.getStrHash(content);
+  },
+
+  /**
+   * NOTE: in git, compressed contents begin with '0x78 0x01',
+   * where '0x78' means zlib compression
+   *
+   * `zlib` here begin with '0x78 0x9c'
+   *
+   * @param {string} content
+   * @param {"blob" | "tree" | "commit"} type
+   * @returns {Buffer}
+   */
+  getStrCompression: (content, type = "blob") => {
+    const str = `${type} ${content.length}\0${content}`;
+    return zlib.deflateSync(str);
+  },
+
+  getFileCompression: (filename, type = "blob") => {
+    const content = fs.readFileSync(filename);
+    return utils.getStrCompression(content, type);
   },
 
   /**
    *
+   * @param {string} gitDir
    * @param {string} hash
-   * @param {"type" | "content" | "contentSize"} mode
+   * @param {'type' | 'content' | 'contentSize'} mode
    * @returns {string}
    */
-  cat_file: (hash, mode, log = true) => {
-    if (!hash || !mode) {
-      console.log("please pass params");
-      return;
+  getFileDecompression: (gitDir, hash, mode) => {
+    const hashFilePath = path.join(
+      process.cwd(),
+      `${gitDir}/objects/${hash.substring(0, 2)}/${hash.substring(
+        hash.length - 38
+      )}`
+    );
+    if (!files.pathExists(hashFilePath)) return "";
+
+    const content = fs.readFileSync(hashFilePath);
+    const deCom = zlib.inflateSync(content).toString();
+    switch (mode) {
+      case "type":
+        return deCom.split(" ")[0];
+      case "content":
+        return deCom.split("\0")[1];
+      case "contentSize":
+        return deCom.split("\0")[0].split(" ")[1];
     }
-    const res = utils.getFileDecompression(gitlet.GIT_DIR, hash, mode);
-    if (res === "") console.log(`${hash} does not math any files`);
-    if (log) console.log(res);
-    return res;
   },
 
   /**
-   * create a tree object of current index
+   *
    * @returns {string}
    */
-  write_tree: () => {
-    const ind = index.read(gitlet.GIT_DIR);
-    const tree = files.nestFlatTree(ind);
-    const treeHash = objects.writeTree(tree);
-    return treeHash;
+  getTime: () => {
+    return new Date().toLocaleString().replaceAll("/", "-");
+  },
+
+  relToAbs: (relPath) => {
+    return path.resolve(process.cwd(), relPath);
   },
 };
 
-module.exports = gitlet;
+module.exports = utils;
